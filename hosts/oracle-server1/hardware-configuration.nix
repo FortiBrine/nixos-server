a838e4771bdeb91518261b12efe60395ece57587
FortiBrine <FortiBrine@users.noreply.github.com>
2026-08-30 03:20:29 +0300

    feat: added zram

--- hosts/oracle-server1/hardware-configuration.nix
+++ hosts/oracle-server1/hardware-configuration.nix
@@ -10,13 +10,36 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/f222513b-ded1-49fa-b591-20ce86a2fe7f";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "systemd-1";
+      fsType = "autofs";
+    };
+
+  zramSwap = {
+    enable = true;
+    memoryPercent = 50;
+    priority = 5;
+  };
+
   swapDevices = [
     {
       device = "/swapfile";
       size = 2048;
+      priority = 0;
     }
   ];
-  boot.kernel.sysctl."vm.swappiness" = 10;
+
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 180;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
