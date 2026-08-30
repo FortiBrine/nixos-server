69dec32bbaf5478176d94a258d51d59c1f9ddbe2
FortiBrine <FortiBrine@users.noreply.github.com>
2026-08-30 18:22:55 +0300

    feat: use netplan renderer

--- profiles/cloud-init-network.nix
+++ profiles/cloud-init-network.nix
@@ -1,9 +1,17 @@
-{
+{ pkgs, ... }: {
+  environment.systemPackages = [ pkgs.netplan ];
+
   networking.useDHCP = false;
   networking.useNetworkd = true;
 
   services.cloud-init = {
     enable = true;
     network.enable = true;
+    settings = {
+      system_info.network = {
+        renderers = [ "netplan" ];
+        activators = [ "netplan" "networkd" ];
+      };
+    };
   };
 }
